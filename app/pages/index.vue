a6ddfecdd84181c5291230003e78717ba8fbc45f
bsa-git <bsa2657@yandex.ru>
2017-09-22 08:52:48 +0300

    Update project

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,64 +1,66 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
+  <section>
+    <div class="jumbotron text-center">
+      <!-- Logo -->
+      <home-logo></home-logo>
+      <!-- Title -->
+      <h1 class="title exotic--light">
         {{ name }}
       </h1>
       <h2 class="subtitle">
         {{ description }}
       </h2>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+        <a href="https://developers.google.com/api-client-library/javascript/start/start-js" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/bsa-git/gapi-examples" target="_blank" class="button--grey">GitHub</a>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+  import HomeLogo from '~/components/home/logo.vue'
 
-export default {
-  components: {
-    Logo
-  },
-  data: function () {
-    return {
-      name: 'Gapi Examples',
-      description: 'Examples Google API'
+  export default {
+    components: {
+      HomeLogo
+    },
+    data: function () {
+      return {
+        name: 'Gapi Examples',
+        description: 'Examples Google API'
+      }
     }
   }
-}
 </script>
 
 <style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+  .button--green {
+    display: inline-block;
+    border-radius: 4px;
+    border: 1px solid #3b8070;
+    color: #3b8070;
+    text-decoration: none;
+    padding: 10px 30px;
+  }
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+  .button--green:hover {
+    color: #fff;
+    background-color: #3b8070;
+  }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+  .button--grey {
+    display: inline-block;
+    border-radius: 4px;
+    border: 1px solid #35495e;
+    color: #35495e;
+    text-decoration: none;
+    padding: 10px 30px;
+    margin-left: 15px;
+  }
 
-.links {
-  padding-top: 15px;
-}
+  .button--grey:hover {
+    color: #fff;
+    background-color: #35495e;
+  }
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -14,11 +14,23 @@ module.exports = {
     title: 'Gapi Examples',
     meta: [
       { charset: 'utf-8' },
+      { 'http-equi': 'X-UA-Compatible', content: 'IE=edge' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Examples Google API' }
+      { hid: 'description', name: 'description', content: 'Examples Google API' },
+      { name: 'author', content: 'bsa-git <bsa2657@yandex.ru>' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/assets/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/assets/favicon.ico' },
+      // Bootstrap core CSS
+      {rel: 'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css', integrity: 'sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u', crossorigin: 'anonymous'},
+      // Bootstrap theme
+      {rel: 'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css', integrity: 'sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp', crossorigin: 'anonymous'}
+    ],
+    script: [
+      // JQuery
+      {src: 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js'},
+      // Bootstrap
+      {src: 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js', integrity: 'sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa', crossorigin: 'anonymous'}
     ]
   },
   /*
@@ -45,5 +57,8 @@ module.exports = {
         })
       }
     }
-  }
+  },
+  css: [
+    {src: '~assets/css/style.css'}
+  ]
 }

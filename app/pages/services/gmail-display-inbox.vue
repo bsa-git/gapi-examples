f5aa57516aae95b708ef2113b5952d69cbd35877
bsa-git <bsa2657@yandex.ru>
2017-10-06 10:08:21 +0300

    Update project

--- app/pages/services/gmail-display-inbox.vue
+++ app/pages/services/gmail-display-inbox.vue
@@ -1,6 +1,5 @@
 <template>
   <section>
-
     <div class="text-center">
       <h2>{{ title }}</h2>
       <p class="lead">{{ description }}</p>
@@ -12,35 +11,90 @@
       <strong>HTTP request</strong>
       <p><code>GET https://www.googleapis.com/gmail/v1/users/userId/messages</code></p>
     </div>
+    <button type="button" class="btn btn-primary" v-if="!isSignedIn" @click="apiGoogle.handleAuthClick(apiGoogle)">
+      Authorize
+    </button>
 
-    <div class="jumbotron">
-
-      <button type="button" class="btn btn-primary" v-if="!isSignedIn" @click="apiGoogle.handleAuthClick(apiGoogle)">
-        Authorize
-      </button>
-
-      <div v-if="isSignedIn">
-        <div class="lead">My Inbox:</div>
-        <table class="table table-striped table-inbox">
-          <thead>
-          <tr>
-            <th>From</th>
-            <th>Subject</th>
-            <th>Date/Time</th>
+    <div v-if="isSignedIn">
+      <h1>Gmail API demo</h1>
+      <!-- Inbox message list  -->
+      <table class="table table-striped table-inbox">
+        <thead>
+        <tr>
+          <th>From</th>
+          <th>Subject</th>
+          <th>Date/Time</th>
+        </tr>
+        </thead>
+        <tbody>
+        <template v-for="message in google.gmail.inbox">
+          <tr :key="message.id">
+            <td v-text="message.from"></td>
+            <td><a :href="`#message-modal-${message.id}`" data-toggle="modal" :id="`message-link-${message.id}`"
+                   v-text="message.subject"></a></td>
+            <td>{{ message.date }}</td>
           </tr>
-          </thead>
-          <tbody>
-          <template v-for="message in google.gmail.inbox">
-            <tr :key="message.id">
-              <td v-html="message.from"></td>
-              <td><a :href="`#message-modal-${message.id}`" data-toggle="modal" :id="`message-link-${message.id}`" v-html="message.subject"></a></td>
-              <td>{{ message.date }}</td>
-            </tr>
-          </template>
-          </tbody>
-        </table>
-      </div>
-
+        </template>
+        </tbody>
+      </table>
+      <!-- Modal window for message body -->
+      <template v-for="message in google.gmail.inbox">
+        <div class="modal fade" :id="`message-modal-${message.id}`" tabindex="-1" role="dialog"
+             aria-labelledby="myModalLabel" :key="message.id">
+          <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button"
+                        class="close"
+                        data-dismiss="modal"
+                        aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+                <h4 class="modal-title" id="myModalLabel">{{ message.subject }}</h4>
+              </div>
+              <div class="modal-body">
+                <iframe :id="`message-iframe-${message.id}`" srcdoc="<p>Loading...</p>"></iframe>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </template>
+      <!--
+      '<div class="modal fade" id="message-modal-' + message.id +
+            '" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">\
+          <div class="modal-dialog modal-lg">\
+            <div class="modal-content">\
+              <div class="modal-header">\
+                <button type="button"\
+                        class="close"\
+                        data-dismiss="modal"\
+                        aria-label="Close">\
+                  <span aria-hidden="true">&times;</span></button>\
+                <h4 class="modal-title" id="myModalLabel">' +
+            getHeader(message.payload.headers, 'Subject') +
+            '</h4>\
+          </div>\
+          <div class="modal-body">\
+            <iframe id="message-iframe-' + message.id + '" srcdoc="<p>Loading...</p>">\
+                  </iframe>\
+                </div>\
+                <div class="modal-footer">\
+                  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>\
+                  <button type="button" class="btn btn-primary reply-button" data-dismiss="modal" data-toggle="modal" data-target="#reply-modal"\
+                  onclick="fillInReply(\
+                    \'' + reply_to + '\', \
+                    \'' + reply_subject + '\', \
+                    \'' + getHeader(message.payload.headers, 'Message-ID') + '\'\
+                    );"\
+                  >Reply</button>\
+                </div>\
+              </div>\
+            </div>\
+          </div>'
+      -->
     </div>
   </section>
 </template>

57970b787137ee79d0487da68aa0ebb7ebe813f3
bsa-git <bsa2657@yandex.ru>
2017-10-11 14:30:29 +0300

    Update project

--- app/components/app/header.vue
+++ app/components/app/header.vue
@@ -16,25 +16,27 @@
           <li :class="{ 'active': $route.name == 'index' }">
             <nuxt-link to="/">Home</nuxt-link>
           </li>
-          <li v-if="isAuth" :class="['dropdown', {active: isGroupMenu('services')}]">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown"
-               role="button" aria-haspopup="true" aria-expanded="false">Services <span class="caret"></span></a>
-            <ul class="dropdown-menu">
-              <li class="dropdown-header">Google People API</li>
-              <li>
-                <nuxt-link to="/services/people-about">About</nuxt-link>
-                <nuxt-link to="/services/people-get">My Information</nuxt-link>
-                <nuxt-link to="/services/people-connections">My Connections</nuxt-link>
-              </li>
-              <li role="separator" class="divider"></li>
-              <li class="dropdown-header">Google Gmail API</li>
-              <li>
-                <nuxt-link to="/services/gmail-about">About</nuxt-link>
-                <nuxt-link to="/services/gmail-display-inbox">Display My Inbox</nuxt-link>
-                <nuxt-link to="/services/gmail-send-message">Send message</nuxt-link>
-              </li>
-            </ul>
-          </li>
+          <template v-if="isAuth">
+            <li :class="['dropdown', {active: isGroupMenu('services')}]">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown"
+                 role="button" aria-haspopup="true" aria-expanded="false">Services <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li class="dropdown-header">Google People API</li>
+                <li>
+                  <nuxt-link to="/services/people-about">About</nuxt-link>
+                  <nuxt-link to="/services/people-get">My Information</nuxt-link>
+                  <nuxt-link to="/services/people-connections">My Connections</nuxt-link>
+                </li>
+                <li role="separator" class="divider"></li>
+                <li class="dropdown-header">Google Gmail API</li>
+                <li>
+                  <nuxt-link to="/services/gmail-about">About</nuxt-link>
+                  <nuxt-link to="/services/gmail-display-inbox">Display My Inbox</nuxt-link>
+                  <nuxt-link to="/services/gmail-send-message">Send message</nuxt-link>
+                </li>
+              </ul>
+            </li>
+          </template>
           <li :class="{ 'active': $route.name == 'login' }">
             <nuxt-link to="/login">{{ isAuth? 'Logout':'Login' }}</nuxt-link>
           </li>
